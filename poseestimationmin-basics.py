deb1d1feb6f95472692796648f5aca6d6717c46b
g-serban <gserban1233@gmail.com>
2021-06-11 20:11:17 +0300

    reuploading project files

---
+++ poseestimationmin-basics.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+
+import time
+
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_draw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture('PoseVideos/1.mp4')
+
+previous_time = 0
+
+while True:
+    success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(img_rgb)
+
+    if results.pose_landmarks:
+        mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        for id, landmark in enumerate(results.pose_landmarks.landmark):
+            height, width, c = img.shape
+            print(id, landmark)
+            cx, cy = int(landmark.x * width), int(landmark.y * height)
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+    current_time = time.time()
+    fps = 1 / (current_time-previous_time)
+    previous_time = current_time
+
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
+
+# TODO fps display is not working
+
